cbfdf5321e4f37ab07fb0cf171dcc6951f4e4510
Gerald Bryant <hakeem1106@gmail.com>
2020-02-23 19:03:27 +0000

    fix

--- .netlify/functions/functions.js
+++ .netlify/functions/functions.js
@@ -1,30 +1,14 @@
-require('dotenv').config()
-exports.handler = (event, context, callback)=>{
-    
-   const send = body =>{
-       callback(null, {
-           statusCode:200,
-           body: JSON.stringify(body)
-       })
-   } 
-    
-    const grabRates =()=>{
-        const url =`http://data.fixer.io/api/latest?access_key=${process.env.API_KEY}` 
+require('dotenv').config();
+import fetch from 'node-fetch';
 
-    fetch(url)
-       .then(res =>send(res) )
-       .then(data => {
-           const rates = Object.entries(data.rates)
-           res.send({rates})
-       })
-       .catch(err =>{
-           res.send(err)
-       })
-
-    }
-
-    if(event.httpMethod == 'GET'){
-        grabRates()
-    }
+exports.handler = async(event, context)=>{
+   return fetch(`http://data.fixer.io/api/latest?access_key=${process.env.API_KEY}`, {headers:{ "Accept": "application/json"}})
+   .then(res => res.json())
+   .then(data =>({
+       statusCode:200,
+       body: data.rates
+   }))
+   .then(console.log(body))
+   .catch(error =>({ statusCode: 422, body:String(error)}))
     
 }
