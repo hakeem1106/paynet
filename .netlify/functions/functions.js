1e2f477bfcb89475a715be9c414411ed023714ac
Gerald Bryant <hakeem1106@gmail.com>
2020-02-23 19:39:54 +0000

    fixing

--- .netlify/functions/functions.js
+++ .netlify/functions/functions.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch')
 exports.handler = async(event, context)=>{
 
    return fetch(`http://data.fixer.io/api/latest?access_key=${process.env.API_KEY}`, {headers:{ "Accept": "application/json"}})
-   .then(res => res.json())
+   .then(response => response.json())
    .then(data =>({
        statusCode:200,
        body: data.rates
