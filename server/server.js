4bf031a0e0a579662bf2f932c8a4bd1d7b66eb32
Gerald <hakeem1106@gmail.com>
2020-02-23 01:10:37 -0500

    updated build command in toml

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const express = require('express');
 const next = require('next');
 const port= 5000;
-
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({dev})
 const handle = app.getRequestHandler()
