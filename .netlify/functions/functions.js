0484e200a836523702f6771517cd96bddbf1df45
Gerald Bryant <hakeem1106@gmail.com>
2020-02-23 22:04:36 +0000

    fixing display

--- .netlify/functions/functions.js
+++ .netlify/functions/functions.js
@@ -12,8 +12,8 @@ exports.handler = (event, context, callback)=>{
    
     const getRates =()=>{
         fetch(`http://data.fixer.io/api/latest?access_key=${process.env.API_KEY}`)
-        .then(res => send(res.data))
-        .catch(err = send(err))
+        .then(res => send(res))
+        console.log(res)
 
     }
 
