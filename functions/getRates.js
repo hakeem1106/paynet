18b4f6215fd59915f1bf484ee2b5a1a67a5e8fcd
Gerald Bryant <hakeem1106@gmail.com>
2020-02-24 23:02:13 +0000

    upating folder

---
+++ functions/getRates.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const axios = require('axios')
+
+
+exports.handler = (event, context, callback)=>{
+
+   axios.get(`http://data.fixer.io/api/latest?access_key=${process.env.API_KEY}`)
+    .then(({data:{ data: rates}})=>{
+        callback(null,{
+            statusCode:200,
+            headers:{
+                "Access-Control-Allow-Origin" : "*",
+                "Access-Control-Allow-Headers": "Content-Type",
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(rates)
+        })
+    })
+    .catch(err=> console.log(err))
+    
+}
