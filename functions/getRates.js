377333ced49e99f8e5768bcba5b5a9a85da96e6f
Gerald <hakeem1106@gmail.com>
2020-02-27 01:53:33 -0500

    fixes

--- functions/getRates.js
+++ functions/getRates.js
@@ -1,25 +1,26 @@
 require('dotenv').config();
-const axios = require('axios')
+const axios = require('axios').default
 
 
 exports.handler = async(event, context, callback)=>{
     
-    getRates= async()=>{
-        axios.get(`http://data.fixer.io/api/latest?access_key=${process.env.API_KEY}`)
-    .then(({data:{ data: rates}})=>{
-        callback(null,{
-            statusCode:200,
-            headers:{
+    getRates = async()=>{
+       await axios.get(`http://data.fixer.io/api/latest?access_key=${process.env.API_KEY}`)
+        .then(res =>res.json())
+        .then(console.log(res))
+        .then(callback(null, {
+            statusCode: 200,
+            headers: {
                 "Access-Control-Allow-Origin" : "*",
                 "Access-Control-Allow-Headers": "Content-Type",
                 'content-type': 'application/json'
-            },
-            body: data
-        })
-    })
-    .catch(err=> console.log(err))
-
+            }
+        
+        }))
     }
-   getRates()
     
+        
+    
+
+    getRates()
 }
