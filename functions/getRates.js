073263043276d248b800d9d680138a3ebef3b869
Gerald <hakeem1106@gmail.com>
2020-02-29 03:37:40 -0500

    function works

--- functions/getRates.js
+++ functions/getRates.js
@@ -3,24 +3,35 @@ const axios = require('axios').default
 
 
 exports.handler = async(event, context, callback)=>{
-    
-    getRates = async()=>{
-       await axios.get(`http://data.fixer.io/api/latest?access_key=${process.env.API_KEY}`)
-        .then(res =>res.json())
-        .then(console.log(res))
-        .then(callback(null, {
+    const send = body=>{
+        callback(null, {
             statusCode: 200,
             headers: {
                 "Access-Control-Allow-Origin" : "*",
                 "Access-Control-Allow-Headers": "Content-Type",
                 'content-type': 'application/json'
-            }
+            },
+            body: JSON.stringify(body)
         
-        }))
+        }) 
+
     }
     
+
+    const getRates = async()=>{
+       await axios.get(`http://data.fixer.io/api/latest?access_key=${process.env.API_KEY}`)
+       .then(res=> send(res.data))
+        .catch(err => send(err))
         
+        
+    }
+    if(event.httpMethod == 'GET'){
+        getRates()
+    }
+    
+    
+      
     
 
-    getRates()
+    
 }
