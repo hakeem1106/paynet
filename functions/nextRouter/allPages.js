b24dbc8360948e7bfd42a10e3e54cf574587afdd
Gerald <hakeem1106@gmail.com>
2020-03-02 00:08:49 -0500

    next-on-netlify

---
+++ functions/nextRouter/allPages.js
@@ -0,0 +1,1 @@
+require("./pages/_error.js")
--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 const withSass = require('@zeit/next-sass')
 module.exports = withSass({
   /* config options here */
+  target: 'serverless',
+  distDir: '.next',
   cssModules: true
 })
