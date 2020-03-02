2b749652e107fdbf4a7e9d5ac0729d1824bfb80f
Gerald <hakeem1106@gmail.com>
2020-03-02 01:34:35 -0500

    changing page function

--- pages/index.js
+++ pages/index.js
@@ -8,16 +8,26 @@ import fetch from 'isomorphic-unfetch'
 class Home extends React.Component{
 constructor(props){
     super(props)
-    this.state={}
+    this.state={
+      rates: 
+      <div>
+        Starter data
+      </div>
+    }
 }
 
-fetchRates = async()=>{
+fetchRates = async(event)=>{
     await (fetch(`/.netlify/functions/getRates`))
     .then(res => res.json())
     .then(res => this.setState({
-      rates: res.rates
+      rates: 
+      <div>
+      {res.rates}
+      </div>
+      
     }))
 }
+
 
  render(){
 
